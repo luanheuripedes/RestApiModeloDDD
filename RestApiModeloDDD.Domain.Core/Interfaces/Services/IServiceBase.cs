cf019c6c4ed5502339f070c5b7a3719ef05e8d7f
luanheuripedes <luanheuripedesmoreiradepaula@gmail.com>
2021-12-28 21:06:03 -0300

    Implementado a interface da Service

---
+++ RestApiModeloDDD.Domain.Core/Interfaces/Services/IServiceBase.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RestApiModeloDDD.Domain.Core.Interfaces.Services
+{
+    public interface IServiceBase<TEntity> where TEntity : class
+    {
+        void Add(TEntity obj);
+
+        void Update(TEntity obj);
+
+        void Remove(TEntity obj);
+
+        IEnumerable<TEntity> GetAll();
+
+        TEntity GetById(int id);
+    }
+}
--- RestApiModeloDDD.Infrastructure/Data/Repositorys/RepositoryProduto.cs
+++ RestApiModeloDDD.Infrastructure/Data/Repositorys/RepositoryProduto.cs
@@ -1,12 +1,9 @@
 ﻿using RestApiModeloDDD.Domain.Core.Interfaces.Repositorys;
 using RestApiModeloDDD.Domain.Entitys;
-using System;
-using System.Collections.Generic;
-using System.Text;
 
 namespace RestApiModeloDDD.Infrastructure.Data.Repositorys
 {
-    public  class RepositoryProduto : RepositoyBase<Produto>, IRepositoryProduto
+    public class RepositoryProduto : RepositoyBase<Produto>, IRepositoryProduto
     {
         private readonly SqlContext sqlContext;
 
